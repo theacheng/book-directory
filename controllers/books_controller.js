7696ecc4138f28aae78a3735786c1f4078dc4893
theacheng <theacheng.g6@gmail.com>
2021-08-26 01:01:48 +0700

    refactor: improve book listing

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -4,39 +4,81 @@ import jsonapiSerializer from 'jsonapi-serializer';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 var JSONAPISerializer = jsonapiSerializer.Serializer;
+const bookAttributes = ['title', 'description', 'authur', 'published_date', 'created_at', 'updated_at', 'properties'];
+const relationshipAttributes = (_included) => {
+  const includes = _included.split(',');
+  const map = {
+    properties: {
+      ref: "id",
+      included: true,
+      attributes: ['name', 'value']
+    },
+    authur: {
+      ref: "id",
+      included: true,
+      attributes: ['first_name', 'last_name']
+    },
+  }
+  const relationships = Object.keys(map).reduce((object, key) => {
+    if (!includes.includes(key)) {
+      object[key] = map[key]
+    }
+    return object
+  }, {})
+  return relationships
+}
 
 const fetchBooks = () => asyncHandler(async (req, res) => {
-  const per_page = parseInt(req.query.per_page) || 20;
-  const page = parseInt(req.query.page) || 0;
+  const per_page = parseInt(req.query.per_page) || 20
+  const page = parseInt(req.query.page) || 0
+  const included = req.query.included
 
   const options = {
     page: page,
     limit: per_page,
+    populate: "authur",
   };
 
-  const books = await Book.paginate({}, options);
-
+  const books = await Book.paginate({}, options)
   const BooksSerializer = new JSONAPISerializer('books', {
-    attributes: ['title', 'description', 'authur', 'published_date', 'created_at', 'updated_at', 'properties'],
+    attributes: bookAttributes,
+    pagination: true,
     meta: {
       count: books.limit,
       total_count: books.totalDocs,
       total_pages: books.totalPages,
       current_page: books.page,
     },
-    properties: {
-      ref: "id",
-      included: true,
-      attributes: ['name', 'value']
-    },
     topLevelLinks: {
       self: books.page,
       next: books.nextPage,
     },
+    ...relationshipAttributes
   });
 
-  const _books = BooksSerializer.serialize(books.docs);;
+  const _books = BooksSerializer.serialize(books.docs)
   res.send(JSON.stringify(_books))
 })
 
-export { fetchBooks }
+const fetchBookDetail = asyncHandler(async (req, res) => {
+  const id = req.params.id
+
+  if (!id) {
+    res.send({ message: 'id not found' })
+    return
+  }
+
+  try {
+    const book = await Book.findById(id).populate('authur')
+    const BooksSerializer = new JSONAPISerializer('book', {
+      attributes: bookAttributes,
+      ...relationshipAttributes
+    });
+    const _book = BooksSerializer.serialize(book);;
+    res.send(JSON.stringify(_book))
+  } catch (error) {
+    res.send({ message: "id not found" })
+  }
+})
+
+export { fetchBooks, fetchBookDetail }
--- routes/book_routes.js
+++ routes/book_routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {
   fetchBooks,
+  fetchBookDetail
 } from '../controllers/books_controller.js'
 
 const router = express.Router();
 
 router.route('/').get(fetchBooks())
+router.route('/:id').get(fetchBookDetail)
 
 export default router;
